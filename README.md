404fbe7c2f090320776fd916753b9a37f1bbcacf
Heinenen <th.m.heinen@gmail.com>
2024-03-22 16:11:44 +0100

    don't replace codeblock with render result

--- README.md
+++ README.md
@@ -1,8 +1,40 @@
 # discourse-typst
 
 ````
-```typst
+```render-typst
 ${ x in RR | x "is natural"and x < 10 }$
 ```
 ````
 
+# TODO
+- [x] don't replace code block when rendering
+    - [ ] nicer styling
+    - [ ] configurable
+        - [ ] maybe with user option to show/copy the source code from context menu
+- [ ] add loading animation if rendering is taking longer
+- [ ] loading anumation if comiler takes longer to download
+- [ ] multiple output formats
+    - [ ] optionally with multiple wasm file to not bloat the main one
+    - [ ] way to download PDF file
+- [ ] configurable preamble
+- [ ] configurable code tag (instead of `render-typst`)
+- [ ] configurable size of render
+    - [ ] maybe with a collapsible part
+    - [ ] maybe with a scroll wheel
+- [ ] handle multiple pages
+- [ ] way to handle uploaded typst files instead of code blocks
+    - [ ] handle other uploaded file/assets like images
+- [ ] more fonts, best to have same fonts for all users
+    - [ ] option for admin to add more?
+- [ ] Typst package support
+- [ ] Typst "playground" support, meaning link to prefilled Typst project
+- [ ] multiple files support
+    - [ ] not only typst file but also bib files and csv etc.
+- [ ] better theme support
+- [ ] add buttons to code block
+    - [ ] render button
+    - [ ] download as PDF/PNG/SVG/ZIP button
+    - [ ] download code button
+    - [ ] print pdf button
+    - [ ] "try it out yourself" button that redirects to playground-ish site
+        - [ ] this redirects to playground with all project files included
--- javascripts/discourse/api-initializers/typst.js
+++ javascripts/discourse/api-initializers/typst.js
@@ -10,32 +10,14 @@ let webWorker;
 const wasmUrl = settings.theme_uploads.wasm;
 
 async function applyTypst(element, key = "composer") {
-  let typst_blocks = element.querySelectorAll("pre[data-code-wrap=typst]");
-
+  let typst_blocks = element.querySelectorAll("pre[data-code-wrap=render-typst")
   if (!typst_blocks.length) {
     return;
   }
 
-  typst_blocks.forEach((block) => {
-    if (block.dataset.processed) {
-      return;
-    }
+  // TODO add spinner if rendering/downloading compiler takes too long
+  // svgbob implements something similar
 
-    const spinner = document.createElement("div");
-    spinner.classList.add("spinner");
-
-    if (block.dataset.codeHeight && key !== "composer") {
-      block.style.height = `${block.dataset.codeHeight}px`;
-    }
-
-    later(() => {
-      if (!block.dataset.processed) {
-        block.append(spinner);
-      }
-    }, 2000);
-  });
-
-  typst_blocks = element.querySelectorAll("pre[data-code-wrap=typst]");
   typst_blocks.forEach(async (block, index) => {
     if (block.dataset.processed) {
       return;
@@ -51,12 +33,10 @@ async function applyTypst(element, key = "composer") {
     let cooked = await cookTypst(code.innerText);
 
     block.dataset.processed = "true";
-    block.innerHTML = stripStyle(cooked);
-
-    if (key === "composer") {
-      later(() => updateMarkdownHeight(block, index), 1000);
-    }
-  });
+    let div = document.createElement("div")
+    div.innerHTML = cooked
+    block.appendChild(div);
+  }); 
 }
 
 let messageSeq = 0;
@@ -86,53 +66,6 @@ async function cookTypst(text) {
   });
 
   return promise;
-}
-
-function stripStyle(svg) {
-  // return svg.replace(/<style.*<\/style>/s, "");
-  return svg
-}
-
-function updateMarkdownHeight(block, index) {
-  let height = parseInt(block.getBoundingClientRect().height);
-  let calculatedHeight = parseInt(block.dataset.calculatedHeight);
-
-  if (height === 0) {
-    return;
-  }
-
-  if (height !== calculatedHeight) {
-    block.dataset.calculatedHeight = height;
-    // TODO: need to use API here
-    let composer = document.getElementsByClassName("d-editor-input")[0];
-    let old = composer.value;
-
-    let split = old.split("\n");
-
-    let n = 0;
-    for (let i = 0; i < split.length; i++) {
-      if (split[i].match(/```typst/)) {
-        if (n === index) {
-          split[i] = "```typst height=" + height;
-        }
-        n += 1;
-      }
-    }
-
-    let joined = split.join("\n");
-
-    if (joined !== composer.value) {
-      let restorePosStart = composer.selectionStart;
-      let restorePosEnd = composer.selectionEnd;
-
-      composer.value = joined;
-
-      if (restorePosStart) {
-        composer.selectionStart = restorePosStart;
-        composer.selectionEnd = restorePosEnd;
-      }
-    }
-  }
 }
 
 export default apiInitializer("1.13.0", (api) => {
